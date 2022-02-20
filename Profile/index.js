6a2257290b255dcbf3df4568e65d8d45e233cab7
deluniee <96617164+deluniee@users.noreply.github.com>
2022-02-21 03:55:18 +0600

    Add files via upload

---
+++ Profile/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    $("#box").height(800);
+    $("#box").width(600);
+  });
+});
+
+
+
+$(document).ready(function(){
+  $("#btn2").click(function(){
+    $("#box").height(100);
+    $("#box").width(100);
+  });
+});
